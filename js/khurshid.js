bd1f44c27f4342a326ffa8545b0be87384d3253b
DESKTOP-MEULOBA\khurshid <khurshid135@gmail.com>
2016-12-11 05:17:22 +0500

    hireme added javascript

---
+++ js/khurshid.js
@@ -0,0 +1,12 @@
+
+function fover() {
+    document.getElementById("no-button").style.visibility = 'hidden';
+}
+
+function fout() {
+    document.getElementById("no-button").style.visibility = 'visible';
+}
+
+function yesclick() {
+    alert("Good Decision!  Here is my email: Khurshid135@gmail.com");
+}
